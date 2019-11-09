2a5ca3761198ab60fb99f02b7634f27f71cbdaf7
credus <aaron.wantoch@gmail.com>
2019-11-09 20:06:25 +0100

    Pathfind

--- Assets/PathFinder.cs
+++ Assets/PathFinder.cs
@@ -1,18 +1,96 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PathFinder : MonoBehaviour
 {
-    Dictionary<Vector2, Waypoint> grid = new Dictionary<Vector2, Waypoint>();
+    Dictionary<Vector2Int, Waypoint> grid = new Dictionary<Vector2Int, Waypoint>();
+    Queue<Waypoint> queue = new Queue<Waypoint>();
+    Waypoint searchCenter;
+
+    bool isRunning = true;
+
+    Vector2Int[] directions =
+    {
+        Vector2Int.up,
+        Vector2Int.right,
+        Vector2Int.down,
+        Vector2Int.left
+    };
+
 
     [SerializeField] Waypoint startWaypoint, endWaypoint;
 
     // Start is called before the first frame update
     void Start()
     {
-        SetColorStartAndEnd();
-        LoadBlocks();
+        if(isRunning)
+        {
+            SetColorStartAndEnd();
+            LoadBlocks();
+            PathFind();
+        }
+        
+    }
+
+    private void PathFind()
+    {
+        queue.Enqueue(startWaypoint);
+        while (queue.Count>0 && isRunning)
+        {
+           searchCenter = queue.Dequeue();
+            print("Searching from " + searchCenter);
+
+            searchCenter.SetTopColor(Color.red);
+            searchCenter.isExplored = true;
+
+            IsEndWaypointFound();
+            ExploreNeighbours();
+        }
+    }
+
+    private void IsEndWaypointFound()
+    {
+        if (searchCenter == endWaypoint)
+        {
+            Waypoint waypoint = searchCenter;
+            while (waypoint.previusWaypoint != null)
+            {
+                print(waypoint);
+                waypoint.SetTopColor(Color.yellow);
+                waypoint = waypoint.previusWaypoint;
+            }
+            isRunning = false;
+        }
+    }
+
+    private void ExploreNeighbours()
+    {
+        if (!isRunning) return;
+        foreach(Vector2Int direction in directions)
+        {
+            AddToQueue(direction);
+        }
+    }
+
+    private void AddToQueue(Vector2Int direction)
+    {
+        Vector2Int neighbourCoordinates = direction + searchCenter.GetWaypointPosition();
+        if (grid.ContainsKey(neighbourCoordinates))
+        {
+            Waypoint neighbour = grid[neighbourCoordinates];
+
+            if (!neighbour.isExplored)
+            {
+                neighbour.SetTopColor(Color.blue);
+                print("Exploring " + neighbour);
+                queue.Enqueue(neighbour);
+
+                neighbour.isExplored = true;
+                neighbour.previusWaypoint = searchCenter;
+            }
+        }
     }
 
     private void SetColorStartAndEnd()
@@ -27,7 +105,7 @@ public class PathFinder : MonoBehaviour
 
         foreach (Waypoint waypoint in waypoints)
         {
-            Vector2 coordinates = waypoint.GetWaypointPosition();
+            Vector2Int coordinates = waypoint.GetWaypointPosition();
 
             if (grid.ContainsKey(coordinates))
             {
--- Assets/Waypoint.cs
+++ Assets/Waypoint.cs
@@ -4,25 +4,16 @@ using UnityEngine;
 
 public class Waypoint : MonoBehaviour
 {
+    public bool isExplored = false;
+    public Waypoint previusWaypoint;
+
     const int gridSize = 11;
 
-    Vector2 waypointPosition;
+    Vector2Int waypointPosition;
 
-    // Start is called before the first frame update
-    void Start()
+    public Vector2Int GetWaypointPosition()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    public Vector2 GetWaypointPosition()
-    {
-        waypointPosition = new Vector2(
+        waypointPosition = new Vector2Int(
             Mathf.RoundToInt(transform.position.x / gridSize),
             Mathf.RoundToInt(transform.position.z / gridSize)
             );
